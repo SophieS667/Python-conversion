09e2dab6d8281edce02ba3ea33e563237ce1cb96
SophieS667 <141285644+SophieS667@users.noreply.github.com>
2023-09-22 16:46:14 -0700

    Conversion
    We imported math so that we could access the code we need. We also worked on it on the same computer, we put an input so the user could type the decimal number they want to convert. Then we switched it to a binary number and we used that number to convert it to a hexadecimal. We then printed both the binary and hexadecimal number.

--- conversion.py
+++ conversion.py
@@ -1,30 +1,8 @@
-<<<<<<< HEAD
-import math 
+
+
+import math
 
 decimal = int(input("What decimal number would you like to convert?"))
 binary = bin(decimal)
 hexadecimal = hex(decimal)
-
-print("Binary =" + binary + " Hexadecimal =" + hexadecimal)
-=======
-import math
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> e7201312fb13228c28c6d0719ec39ffb1cae4900
+print("Binary= " + binary + " Hexadecimal" + hexadecimal)
