4bc2be4acaa6a53b98bb6ce177faa6ebcb59e15e
SophieS667 <141285644+SophieS667@users.noreply.github.com>
2023-09-22 16:31:10 -0700

    Conversion
    We both worked on the same computer and we imported the math library. We also put the input so the user can type a decimal number. Then the code converts it to a binary and line 5 converts that binary to a hexadecimal. Line 7 will print out both numbers.

---
+++ conversion.py
@@ -0,0 +1,7 @@
+import math 
+
+decimal = int(input("What decimal number would you like to convert?"))
+binary = bin(decimal)
+hexadecimal = hex(decimal)
+
+print("Binary =" + binary + " Hexadecimal =" + hexadecimal)
